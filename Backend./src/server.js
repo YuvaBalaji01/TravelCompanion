1a6c207f8585ede95c9c942c38eb823535c30337
Yuva Balaji kumar Yarapathineni <yuvabalajikumar@gmail.com>
2026-01-19 21:53:58 +0530

    Add files via upload

---
+++ Backend./src/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const db = require("./config/db");
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// routes
+const authRoutes = require("./routes/authRoutes");
+app.use("/api/auth", authRoutes);
+
+const tripRoutes = require("./routes/tripRoutes");
+app.use("/api/trips", tripRoutes);
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
+});
