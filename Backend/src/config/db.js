17d813afcb6a04850c010f8bf5d7150b255ddc2b
Yuva Balaji kumar Yarapathineni <yuvabalajikumar@gmail.com>
2026-01-21 16:06:11 +0530

    Update db.js

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -1,21 +1,13 @@
-const mysql = require("mysql2");
-const dotenv = require("dotenv");
-
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
 dotenv.config();
 
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
-db.connect((err) => {
-    if (err) {
-        console.log("❌ MySQL Connection Failed", err);
-    } else {
-        console.log("✅ MySQL Connected Successfully");
-    }
-});
-
-module.exports = db;
+export default pool;
