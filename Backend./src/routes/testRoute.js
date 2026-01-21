b53fcb898e9823084ccb845d4d72ebb448673c6f
Yuva Balaji kumar Yarapathineni <yuvabalajikumar@gmail.com>
2026-01-21 14:43:10 +0530

    Delete Backend. directory

--- Backend./src/routes/testRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { testAPI } = require("../controllers/testController");
-
-router.get("/", testAPI);
-
-module.exports = router;
